0bd4e9d8c0a27a754fbafd431a51e69b2190d2c6
RITESH <riteshtg7@gmail.com>
2026-06-25 23:59:04 +0530

    Fix import paths of capital letters

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,9 +6,9 @@ import { useNavigate, useRoutes } from 'react-router-dom';
 
 import Dashboard from "./components/dashboard/Dashboard"
 import Profile from './components/user/Profile';
-import Login from './components/auth/login';
-import Signup from './components/auth/signup';
-import CreateRepository from './components/repo/createRepo';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import CreateRepository from './components/repo/CreateRepo';
 
 // Auth context
 import { useAuth } from './authContext';
